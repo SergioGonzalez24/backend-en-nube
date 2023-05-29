b3c7bfc973ac60d02a73301c3e56d0029618d083
Sergio Gonzalez <sergiomanuel2020@gmail.com>
2023-05-29 15:31:55 -0600

    controller

--- src/controllers/cuentaController.ts
+++ src/controllers/cuentaController.ts
@@ -1,91 +1,80 @@
-// import { Request, Response } from "express";
-// import { checkSchema } from "express-validator";
-// import AbstractController from "./AbstractController";
-// import db from "../models";
+import { Request, Response } from "express";
+import AbstractController from "./AbstractController";
+import db from "../models";
 
-// class CuentaController extends AbstractController {
-//   protected validateBody(type: any) {
-//     throw new Error("Method not implemented.");
-//   }
+class CuentaController extends AbstractController {
+  protected validateBody(type: any) {
+      throw new Error("Method not implemented.");
+  }
+  private static instance: CuentaController;
 
-//   // Singleton
-//   // Atributo de clase
-//   private static instance: CuentaController;
-//   // Método de clase
-//   public static getInstance(): AbstractController {
-//     if (this.instance) {
-//       return this.instance;
-//     }
-//     this.instance = new CuentaController("cuenta");
-//     return this.instance;
-//   }
+  public static getInstance(): AbstractController {
+    if (this.instance) {
+      return this.instance;
+    }
+    this.instance = new CuentaController("cuenta");
+    return this.instance;
+  }
 
-//   protected initRoutes(): void {
-//     this.router.post("/deposito", this.deposito.bind(this));
-//     this.router.post("/retiro", this.retiro.bind(this));
-//     this.router.get("/saldo", this.saldo.bind(this));
-//   }
+  protected initRoutes(): void {
+    this.router.post("/deposito", this.deposito.bind(this));
+    this.router.post("/retiro", this.retiro.bind(this));
+    this.router.get("/saldo", this.saldo.bind(this));
+  }
 
-//   private async deposito(req: Request, res: Response) {
-//     const { cantidad } = req.body;
-//     try {
-//       // Lógica para realizar un depósito en la cuenta
-//       const saldoActual = await db.Cuenta.findOne({ where: { userId: req.user.id } });
+  private async deposito(req: Request, res: Response) {
+    // Obtener el monto del depósito desde el cuerpo de la solicitud
+    const { monto } = req.body;
 
-//       if (!saldoActual) {
-//         // La cuenta no existe
-//         return res.status(404).send({ message: "Cuenta no encontrada" });
-//       }
+    try {
+      // Lógica para realizar el depósito en la cuenta
+      
+      // Si el depósito fue exitoso, devolver un mensaje de éxito
+      res.status(200).json({ message: "Depósito realizado exitosamente" });
+    } catch (error: any) {
+      res
+        .status(500)
+        .json({ code: error.code, message: error.message })
+        .end();
+    }
+  }
 
-//       const saldoNuevo = saldoActual.saldo + cantidad;
-//       await db.Cuenta.update({ saldo: saldoNuevo }, { where: { userId: req.user.id } });
+  private async retiro(req: Request, res: Response) {
+    // Obtener el monto del retiro desde el cuerpo de la solicitud
+    const { monto } = req.body;
 
-//       res.status(200).send({ message: "Depósito realizado con éxito" });
-//     } catch (error: any) {
-//       res.status(500).send({ code: error.code, message: error.message });
-//     }
-//   }
+    try {
+      // Lógica para realizar el retiro de la cuenta y verificar si hay saldo suficiente
+      
+      // Verificar si hay saldo suficiente en la cuenta
+      if (saldoSuficiente) {
+        // Si hay saldo suficiente, realizar el retiro y devolver un mensaje de éxito
+        res.status(200).json({ message: "Retiro realizado exitosamente" });
+      } else {
+        // Si no hay saldo suficiente, devolver un mensaje de error
+        res.status(400).json({ message: "No hay saldo suficiente" });
+      }
+    } catch (error: any) {
+      res
+        .status(500)
+        .json({ code: error.code, message: error.message })
+        .end();
+    }
+  }
 
-//   private async retiro(req: Request, res: Response) {
-//     const { cantidad } = req.body;
-//     try {
-//       // Lógica para realizar un retiro en la cuenta
-//       const saldoActual = await db.Cuenta.findOne({ where: { userId: req.user.id } });
+  private async saldo(req: Request, res: Response) {
+    try {
+      // Lógica para consultar el saldo de la cuenta
 
-//       if (!saldoActual) {
-//         // La cuenta no existe
-//         return res.status(404).send({ message: "Cuenta no encontrada" });
-//       }
+      // Devolver el saldo actual de la cuenta
+      res.status(200).json({ saldo: saldoActual });
+    } catch (error: any) {
+      res
+        .status(500)
+        .json({ code: error.code, message: error.message })
+        .end();
+    }
+  }
+}
 
-//       if (saldoActual.saldo < cantidad) {
-//         // No hay suficiente saldo en la cuenta
-//         return res.status(400).send({ message: "Saldo insuficiente" });
-//       }
-
-//       const saldoNuevo = saldoActual.saldo - cantidad;
-//       await db.Cuenta.update({ saldo: saldoNuevo }, { where: { userId: req.user.id } });
-
-//       res.status(200).send({ message: "Retiro realizado con éxito" });
-//     } catch (error: any) {
-//       res.status(500).send({ code: error.code, message: error.message });
-//     }
-//   }
-
-//   private async saldo(req: Request, res: Response) {
-//     try {
-//       // Lógica para consultar el saldo de la cuenta
-//       const cuenta = await db.Cuenta.findOne({ where: { userId: req.user.id } });
-
-//       if (!cuenta) {
-//         // La cuenta no existe
-//         return res.status(404).send({ message: "Cuenta no encontrada" });
-//       }
-
-//       res.status(200).send({ saldo: cuenta.saldo });
-//     } catch (error: any) {
-//       res.status(500).send({ code: error.code, message: error.message });
-//     }
-//   }
-// }
-
-// export default CuentaController;
+export default CuentaController;
